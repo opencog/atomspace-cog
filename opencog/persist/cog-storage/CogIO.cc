b0d4a6e645d6544c433641df5b921f822c060ffc
Linas Vepstas <linasvepstas@gmail.com>
2020-07-18 21:18:42 -0500

    One more unit test.

--- opencog/persist/cog-storage/CogIO.cc
+++ opencog/persist/cog-storage/CogIO.cc
@@ -26,7 +26,6 @@
 #include <opencog/atoms/base/Atom.h>
 #include <opencog/atoms/base/Node.h>
 #include <opencog/atoms/base/Link.h>
-#include <opencog/atoms/value/Value.h>
 #include <opencog/atomspace/AtomSpace.h>
 #include <opencog/persist/sexpr/Sexpr.h>
 
