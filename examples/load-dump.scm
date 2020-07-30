3439a93838b8003beca5534e0bbab9b30d05eda3
Linas Vepstas <linasvepstas@gmail.com>
2020-07-30 14:01:59 -0500

    Polish the demo a bit

--- examples/load-dump.scm
+++ examples/load-dump.scm
@@ -14,11 +14,17 @@
 ; store-referers ATOM -- store all graphs that contain ATOM
 ; store-atomspace -- store everything.
 ;
+; -------------------------------
+; Basic initialization and set-up
+;
 (use-modules (opencog) (opencog persist))
 (use-modules (opencog persist-cog))
 
 (cogserver-open "cog://localhost")
 
+; ----------------------------------
+; Load Atoms from the remote server.
+;
 ; Start by assuming the remote server has some content. If not, then
 ; create some. If unsure how, re-read the `fetch-store.scm` demo.
 ; This demo works best if you run it after the `fetch-store.scm` demo,
@@ -40,9 +46,20 @@
 (load-atomspace)
 (cog-get-all-roots)
 
-; -------------------------------------------------
+; Let's take a look at some low-level I/O statistics.
+(cogserver-stats)
+
+; ---------------------------------
+; Store Atoms to the remote server.
+;
 ; Lets create some more atoms, and the store everything.
 (Concept "foo" (stv 0.1 0.2))
 (Concept "bar" (stv 0.3 0.4))
 (Set (List (Set (List (Concept "bazzzz" (stv 0.5 0.6))))))
 (store-atomspace)
+
+; Let's take a look at those low-level I/O stats again.
+(cogserver-stats)
+
+; That's all folks!  Thanks for paying attention!
+; -----------------------------------------------
